440e7afb2fa884ee2eeb2b78d6d7a99848c16e43
RUEI TING HUNG <rthung96@gmail.com>
2024-12-30 18:29:06 +0800

    combine with uart

--- template.c
+++ template.c
@@ -1,17 +1,204 @@
+// CONFIG1H
+#pragma config OSC = INTIO67  // Oscillator Selection bits (HS oscillator)
+#pragma config FCMEN = OFF    // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+#pragma config IESO = ON      // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
+
+// CONFIG2L
+#pragma config PWRT = OFF       // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = SBORDIS  // Brown-out Reset Enable bits (Brown-out Reset enabled in hardware only (SBOREN is disabled))
+#pragma config BORV = 3         // Brown Out Reset Voltage bits (Minimum setting)
+
+// CONFIG2H
+#pragma config WDT = OFF  // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+#pragma config WDTPS = 1  // Watchdog Timer Postscale Select bits (1:1)
+
+// CONFIG3H
+#pragma config CCP2MX = PORTC  // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
+#pragma config PBADEN = ON     // PORTB A/D Enable bit (PORTB<4:0> pins are configured as analog input channels on Reset)
+#pragma config LPT1OSC = OFF   // Low-Power Timer1 Oscillator Enable bit (Timer1 configured for higher power operation)
+#pragma config MCLRE = ON      // MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
+
+// CONFIG4L
+#pragma config STVREN = ON  // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+#pragma config LVP = OFF    // Single-Supply ICSP Enable bit (Single-Supply ICSP enabled)
+#pragma config XINST = OFF  // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
+
+// CONFIG5L
+#pragma config CP0 = OFF  // Code Protection bit (Block 0 (000800-001FFFh) not code-protected)
+#pragma config CP1 = OFF  // Code Protection bit (Block 1 (002000-003FFFh) not code-protected)
+#pragma config CP2 = OFF  // Code Protection bit (Block 2 (004000-005FFFh) not code-protected)
+#pragma config CP3 = OFF  // Code Protection bit (Block 3 (006000-007FFFh) not code-protected)
+
+// CONFIG5H
+#pragma config CPB = OFF  // Boot Block Code Protection bit (Boot block (000000-0007FFh) not code-protected)
+#pragma config CPD = OFF  // Data EEPROM Code Protection bit (Data EEPROM not code-protected)
+
+// CONFIG6L
+#pragma config WRT0 = OFF  // Write Protection bit (Block 0 (000800-001FFFh) not write-protected)
+#pragma config WRT1 = OFF  // Write Protection bit (Block 1 (002000-003FFFh) not write-protected)
+#pragma config WRT2 = OFF  // Write Protection bit (Block 2 (004000-005FFFh) not write-protected)
+#pragma config WRT3 = OFF  // Write Protection bit (Block 3 (006000-007FFFh) not write-protected)
+
+// CONFIG6H
+#pragma config WRTC = OFF  // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) not write-protected)
+#pragma config WRTB = OFF  // Boot Block Write Protection bit (Boot block (000000-0007FFh) not write-protected)
+#pragma config WRTD = OFF  // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
+
+// CONFIG7L
+#pragma config EBTR0 = OFF  // Table Read Protection bit (Block 0 (000800-001FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR1 = OFF  // Table Read Protection bit (Block 1 (002000-003FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR2 = OFF  // Table Read Protection bit (Block 2 (004000-005FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR3 = OFF  // Table Read Protection bit (Block 3 (006000-007FFFh) not protected from table reads executed in other blocks)
+
+// CONFIG7H
+#pragma config EBTRB = OFF  // Boot Block Table Read Protection bit (Boot block (000000-0007FFh) not protected from table reads executed in other blocks)
+
 #include <pic18f4520.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 #include <xc.h>
 #define _XTAL_FREQ 4000000
-#define delay(t) __delay_ms(t * 1000);
+#define STR_MAX 100
 #define VR_MAX ((1 << 10) - 1)
+#define delay(t) __delay_ms(t * 1000);
 
-#pragma config OSC = INTIO67    // Oscillator Selection bits
-#pragma config WDT = OFF        // Watchdog Timer Enable bit 
-#pragma config PWRT = OFF       // Power-up Enable bit
-#pragma config BOREN = ON       // Brown-out Reset Enable bit
-#pragma config PBADEN = OFF     // Watchdog Timer Enable bit 
-#pragma config LVP = OFF        // Low Voltage (single -supply) In-Circute Serial Pragramming Enable bit
-#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit
+// ---------------- Uart --------------------
+
+char mystring[STR_MAX];
+int lenStr = 0;
+
+void UART_Write(unsigned char data)  // Output on Terminal
+{
+    while (!TXSTAbits.TRMT);  // Busy Waiting
+    TXREG = data;             // write to TXREG will send data
+}
+
+void ClearBuffer() {
+    for (int i = 0; i < STR_MAX; i++)
+        mystring[i] = '\0';
+    lenStr = 0;
+}
+
+void MyusartRead() {
+    char data = RCREG;
+    mystring[lenStr++] = RCREG;
+    if (data == '\r') {
+        UART_Write('\r');
+        UART_Write('\n');
+    }else{
+        UART_Write(data);
+    }
+    
+}
+
+char *GetString() {
+    return mystring;
+}
+
+void __interrupt(low_priority) Lo_ISR(void) {
+    if (RCIF) {
+        if (RCSTAbits.OERR) {
+            CREN = 0;
+            Nop();
+            CREN = 1;
+        }
+
+        MyusartRead();
+    }
+
+    // process other interrupt sources here, if required
+    return;
+}
+
+// ---------------- Settings --------------------
+
+void Initialize(void) {
+    // Configure oscillator
+    OSCCONbits.IRCF = 0b110;    // 4 MHz
+    // Configure ADC
+    TRISAbits.RA0 = 1;          // Set RA0 as input port
+    ADCON1bits.PCFG = 0b1110;   // AN0 as analog input
+    ADCON0bits.CHS = 0b0000;    // Select AN0 channel
+    ADCON1bits.VCFG0 = 0;       // Vref+ = Vdd
+    ADCON1bits.VCFG1 = 0;       // Vref- = Vss
+    ADCON2bits.ADCS = 0b000;    // ADC clock Fosc/2
+    ADCON2bits.ACQT = 0b001;    // 2Tad acquisition time
+    ADCON0bits.ADON = 1;        // Enable ADC
+    ADCON2bits.ADFM = 1;        // Right justified
+
+    // Configure servo (PWM)
+    T2CONbits.TMR2ON = 0b1;     // Timer2 on
+    T2CONbits.T2CKPS = 0b11;    // Prescaler 16
+    CCP1CONbits.CCP1M = 0b1100; // PWM mode
+    PR2 = 0x9b;                 // Set PWM period
+    
+    TRISCbits.TRISC2 = 0;
+    
+    // Configure I/O ports
+    TRISA &= 0xF1;              // Set RA1-RA3 as outputs for LED
+    TRISB = 1;                  // RB0 as input for button
+    TRISC = 0;                  // PORTC as output for servo
+    LATA &= 0xF1;               // Clear RA1-RA3
+    LATC = 0;                   // Clear PORTC
+    
+    // Configure interrupts
+    INTCONbits.INT0IF = 0;      // Clear INT0 flag
+    INTCONbits.INT0IE = 1;      // Enable INT0 interrupt
+    PIE1bits.ADIE = 1;          // Enable ADC interrupt
+    PIR1bits.ADIF = 0;          // Clear ADC flag
+    INTCONbits.PEIE = 1;        // Enable peripheral interrupt
+    INTCONbits.GIE = 1;         // Enable global interrupt
+    RCONbits.IPEN = 1;          //enable Interrupt Priority mode
+    INTCONbits.GIEH = 1;        //enable high priority interrupt
+    INTCONbits.GIEL = 1;        //disable low priority interrupt
+    
+    // Configure UART
+    /*
+           TODObasic
+           Serial Setting
+        1.   Setting Baud rate
+        2.   choose sync/async mode
+        3.   enable Serial port (configures RX/DT and TX/CK pins as serial port pins)
+        3.5  enable Tx, Rx Interrupt(optional)
+        4.   Enable Tx & RX
+    */
+    TRISCbits.TRISC6 = 1;  // RC6(TX) : Transmiter set 1 (output)
+    TRISCbits.TRISC7 = 1;  // RC7(RX) : Receiver set 1   (input)
+
+    // Setting Baud rate
+    // Baud rate = 1200 (Look up table)
+    TXSTAbits.SYNC = 0;     // Synchronus or Asynchronus
+    BAUDCONbits.BRG16 = 0;  // 16 bits or 8 bits
+    TXSTAbits.BRGH = 0;     // High Baud Rate Select bit
+    SPBRG = 51;             // Control the period
+
+    // Serial enable
+    RCSTAbits.SPEN = 1;  // Enable asynchronus serial port (must be set to 1)
+    PIR1bits.TXIF = 0;   // Set when TXREG is empty
+    PIR1bits.RCIF = 0;   // Will set when reception is complete
+    TXSTAbits.TXEN = 1;  // Enable transmission
+    RCSTAbits.CREN = 1;  // Continuous receive enable bit, will be cleared when error occured
+    PIE1bits.TXIE = 0;   // Wanna use Interrupt (Transmit)
+    IPR1bits.TXIP = 0;   // Interrupt Priority bit
+    PIE1bits.RCIE = 1;   // Wanna use Interrupt (Receive)
+    IPR1bits.RCIP = 0;   // Interrupt Priority bit
+    /* Transmitter (output)
+     TSR   : Current Data
+     TXREG : Next Data
+     TXSTAbits.TRMT will set when TSR is empty
+    */
+    /* Reiceiver (input)
+     RSR   : Current Data
+     RCREG : Correct Data (have been processed) : read data by reading the RCREG Register
+    */
+    
+    // Start ADC conversion
+    ADCON0bits.GO = 1;
+}
+
+// ---------------- OOP --------------------
 
 int get_LED(){
     return (LATA >> 1);
@@ -73,53 +260,19 @@ void __interrupt(high_priority) H_ISR(){
     
     if(INTCONbits.INT0IF){ // Handle button interrupt
         button_pressed();
-        __delay_ms(10); // bouncing problem
+        __delay_ms(50); // bouncing problem
         INTCONbits.INT0IF = 0;
     }
 }
 
-void init_all(){
-    // Configure oscillator
-    OSCCONbits.IRCF = 0b110;    // 4MHz for servo control
-    
-    // Configure ADC
-    TRISAbits.RA0 = 1;          // Set RA0 as input port
-    ADCON1bits.PCFG = 0b1110;   // AN0 as analog input
-    ADCON0bits.CHS = 0b0000;    // Select AN0 channel
-    ADCON1bits.VCFG0 = 0;       // Vref+ = Vdd
-    ADCON1bits.VCFG1 = 0;       // Vref- = Vss
-    ADCON2bits.ADCS = 0b000;    // ADC clock Fosc/2
-    ADCON2bits.ACQT = 0b001;    // 2Tad acquisition time
-    ADCON0bits.ADON = 1;        // Enable ADC
-    ADCON2bits.ADFM = 1;        // Right justified
-    
-    // Configure servo (PWM)
-    T2CONbits.TMR2ON = 0b1;     // Timer2 on
-    T2CONbits.T2CKPS = 0b11;    // Prescaler 16
-    CCP1CONbits.CCP1M = 0b1100; // PWM mode
-    PR2 = 0x9b;                 // Set PWM period
-    
-    // Configure I/O ports
-    TRISA &= 0xF1;              // Set RA1-RA3 as outputs for LED
-    TRISB = 1;                  // RB0 as input for button
-    TRISC = 0;                  // PORTC as output for servo
-    LATA &= 0xF1;               // Clear RA1-RA3
-    LATC = 0;                   // Clear PORTC
-    
-    // Configure interrupts
-    INTCONbits.INT0IF = 0;      // Clear INT0 flag
-    INTCONbits.INT0IE = 1;      // Enable INT0 interrupt
-    PIE1bits.ADIE = 1;          // Enable ADC interrupt
-    PIR1bits.ADIF = 0;          // Clear ADC flag
-    INTCONbits.PEIE = 1;        // Enable peripheral interrupt
-    INTCONbits.GIE = 1;         // Enable global interrupt
-    
-    // Start ADC conversion
-    ADCON0bits.GO = 1;
-}
+// --------------- TODO ------------------
 
 void button_pressed(){
     // Do sth when the button is pressed
+    /* Example: 
+     * set_LED(get_LED() + 1);
+     */
+    
     
 }
 
@@ -130,10 +283,25 @@ void variable_register_changed(int value){ // value: 0 ~ 1023
      * set_LED_analog(VR_value_to_LED_analog(value));
      */
     
+    
+}
+
+void keyboard_input(char *str){ // str: the words you type on the keyboard
+    // Do sth when typing on keyboard
+    /* Example:
+    for (int i = 0; i < strlen(str); i++) {
+        if (isdigit(str[i])) {
+            int value = (int)str[i] - 48;
+            set_LED_analog(value * 100);
+        }
+    }
+     */
+    
+    
 }
 
 void main(){
-    init_all();
+    Initialize();
     /* Usage:
      * set_servo_angle(-90); // input: -90 ~ 90
      * get_servo_angle(); // return value: -90 ~ 90
@@ -148,11 +316,19 @@ void main(){
      * VR_value_to_LED_analog(1024); // return value: 0 ~ 1024. Change the variable register value to LED brightness
      * 
      * delay(1); // delay 1 second
+     * 
+     * ClearBuffer(); // clear buffer
      */
+    
+    char str[STR_MAX];
     
     while(1) {
         // Do sth in main
         
+        
+        
+        strcpy(str, GetString());
+        if(strlen(str)) keyboard_input(str);
         if(ADCON0bits.GO == 0) ADCON0bits.GO = 1;
     }
 }
