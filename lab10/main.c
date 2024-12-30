665ed641b9597a5437eaa276404da7d2d7f94127
erichung9060 <rthung96@gmail.com>
2024-12-30 14:24:54 +0800

    finished lab10

--- lab10/main.c
+++ lab10/main.c
@@ -1,93 +1,75 @@
-/*
-Author: monkeyyo1104
-Date: 2024/12/30
-*/
+#include <ctype.h>
+#include <stdlib.h>
 
 #include "setting_hardaware/setting.h"
-#include <stdlib.h>
-#include <ctype.h>
 #include "stdio.h"
 #include "string.h"
-#define _XTAL_FREQ 4000000 // 4M Hz
+#define _XTAL_FREQ 4000000  // 4M Hz
 // using namespace std;
+#define STR_MAX 100
 
-char str[20];
-int interrupt = 0;
+char str[STR_MAX];
+int is_interrupt = 0;
 
-void Mode1(){   // Todo : Mode1 
-    int cur = 2;
-    while(cur < 20){
+void Mode1() {  // Todo : Mode1
+    while (1) {
         strcpy(str, GetString());
-        if(isdigit(str[cur]))
-            LATA = (int)str[cur++] - 48;
-    }
-    return ;
-}
-void Mode2(){   // Todo : Mode2
-    int cur = 2;
-    while(cur < 20){
-        strcpy(str, GetString());
-        if(str[cur] == '\r' || str[cur] == ' '){
-            LATA = 0;
-            return;
-        }
-        else if(str[cur] != '\0'){
-            int terminal = (int)str[cur++] - 48;
-            int counter = 0;
-            while(1){
-                if(interrupt)
-                    break;
-                strcpy(str, GetString());
-                if(str[cur] != '\0') 
-                    break;
-                LATA = counter++;
-                __delay_ms(500); // 0.5s
-                if(counter == terminal + 1)
-                    counter = 0;
+        ClearBuffer();
+        for (int i = 0; i < strlen(str); i++) {
+            if (isdigit(str[i])) {
+                LATA = (((int)str[i] - 48) << 1);
             }
-            interrupt = 0;
         }
     }
-    return ;
 }
-void main(void) 
-{
-    
-    SYSTEM_Initialize() ;
-    
-    while(1) {
-        strcpy(str, GetString()); // TODO : GetString() in uart.c
-        for(int i = 0 ; i < 20 ; i++){
-            if(str[i] == '\0')
-                break;
-            else if(str[i] == '\r' || str[i] == ' '){
+
+void Mode2() {  // Todo : Mode2
+    while (1) {
+        strcpy(str, GetString());
+        ClearBuffer();
+        for (int i = 0; i < strlen(str); i++) {
+            if (isdigit(str[i])) {
+                int val = (int)str[i] - 48;
+                is_interrupt = 0;
+                int now = val;
+                while (!is_interrupt) {
+                    LATA = now << 1;
+                    __delay_ms(500);
+                    now--;
+                    if (now < 0) now = val;
+                }
+            }
+        }
+    }
+}
+void main() {
+    SYSTEM_Initialize();
+
+    while (1) {
+        strcpy(str, GetString());  // TODO : GetString() in uart.c
+
+        for (int i = 0; i + 1 < strlen(str); i++) {
+            if (str[i] == 'm' && str[i + 1] == '1') {  // Mode1
                 ClearBuffer();
-                break;
+                Mode1();
+            } else if (str[i] == 'm' && str[i + 1] == '2') {  // Mode2
+                ClearBuffer();
+                Mode2();
             }
         }
-        if(str[0] =='m' && str[1] =='1'){ // Mode1
-            Mode1();
-            ClearBuffer();
-        }
-        else if(str[0] =='m' && str[1] =='2'){ // Mode2
-            Mode2();
-            ClearBuffer();  
-            interrupt = 0;
-        }   
     }
     return;
 }
 
-void __interrupt(high_priority) Hi_ISR(void)
-{
-    if(INTCONbits.INT0IF == 1){
+void __interrupt(high_priority) Hi_ISR(void) {
+    if (INTCONbits.INT0IF == 1) {
         LATA = 0;
-        interrupt = 1;
+        is_interrupt = 1;
         // Disable Interrupt
-        INTCONbits.INT0IF = 0; // clear Interrupt flag bit (Interrupt 0 (Same as RB0 pin))
-        __delay_ms(250); // 0.25s
+        INTCONbits.INT0IF = 0;  // clear Interrupt flag bit (Interrupt 0 (Same as RB0 pin))
+        __delay_ms(250);        // 0.25s
         return;
     }
-    __delay_ms(250); // 0.25s
+    __delay_ms(250);  // 0.25s
     return;
 }
--- lab10/uart.c
+++ lab10/uart.c
@@ -4,41 +4,42 @@ Date: 2024/12/30
 */
 
 #include <xc.h>
-    //setting TX/RX
+// setting TX/RX
+#define STR_MAX 100
 
-char mystring[20];
+char mystring[STR_MAX];
 int lenStr = 0;
 
 void UART_Initialize() {
-    /*     
-           TODObasic   
-           Serial Setting      
+    /*
+           TODObasic
+           Serial Setting
         1.   Setting Baud rate
-        2.   choose sync/async mode 
+        2.   choose sync/async mode
         3.   enable Serial port (configures RX/DT and TX/CK pins as serial port pins)
         3.5  enable Tx, Rx Interrupt(optional)
         4.   Enable Tx & RX
-    */       
-    TRISCbits.TRISC6 = 1;    // RC6(TX) : Transmiter set 1 (output)        
-    TRISCbits.TRISC7 = 1;    // RC7(RX) : Receiver set 1   (input)
-    
+    */
+    TRISCbits.TRISC6 = 1;  // RC6(TX) : Transmiter set 1 (output)
+    TRISCbits.TRISC7 = 1;  // RC7(RX) : Receiver set 1   (input)
+
     // Setting Baud rate
     // Baud rate = 1200 (Look up table)
-    TXSTAbits.SYNC = 0;    // Synchronus or Asynchronus       
-    BAUDCONbits.BRG16 = 0; // 16 bits or 8 bits 
-    TXSTAbits.BRGH = 0;    // High Baud Rate Select bit
-    SPBRG = 51;            // Control the period
-    
-   // Serial enable
-    RCSTAbits.SPEN = 1;  // Enable asynchronus serial port (must be set to 1)              
-    PIR1bits.TXIF  = 0;  // Set when TXREG is empty
-    PIR1bits.RCIF  = 0;  // Will set when reception is complete
-    TXSTAbits.TXEN = 1;  // Enable transmission         
-    RCSTAbits.CREN = 1;  // Continuous receive enable bit, will be cleared when error occured           
-    PIE1bits.TXIE  = 0;  // Wanna use Interrupt (Transmit)    
-    IPR1bits.TXIP  = 0;  // Interrupt Priority bit         
-    PIE1bits.RCIE  = 1;  // Wanna use Interrupt (Receive)       
-    IPR1bits.RCIP  = 0;  // Interrupt Priority bit
+    TXSTAbits.SYNC = 0;     // Synchronus or Asynchronus
+    BAUDCONbits.BRG16 = 0;  // 16 bits or 8 bits
+    TXSTAbits.BRGH = 0;     // High Baud Rate Select bit
+    SPBRG = 51;             // Control the period
+
+    // Serial enable
+    RCSTAbits.SPEN = 1;  // Enable asynchronus serial port (must be set to 1)
+    PIR1bits.TXIF = 0;   // Set when TXREG is empty
+    PIR1bits.RCIF = 0;   // Will set when reception is complete
+    TXSTAbits.TXEN = 1;  // Enable transmission
+    RCSTAbits.CREN = 1;  // Continuous receive enable bit, will be cleared when error occured
+    PIE1bits.TXIE = 0;   // Wanna use Interrupt (Transmit)
+    IPR1bits.TXIP = 0;   // Interrupt Priority bit
+    PIE1bits.RCIE = 1;   // Wanna use Interrupt (Receive)
+    IPR1bits.RCIP = 0;   // Interrupt Priority bit
     /* Transmitter (output)
      TSR   : Current Data
      TXREG : Next Data
@@ -47,61 +48,50 @@ void UART_Initialize() {
     /* Reiceiver (input)
      RSR   : Current Data
      RCREG : Correct Data (have been processed) : read data by reading the RCREG Register
-    */ 
-    }
+    */
+}
 
 void UART_Write(unsigned char data)  // Output on Terminal
 {
-    while(!TXSTAbits.TRMT);    // Busy Waiting
-    TXREG = data;              // write to TXREG will send data 
+    while (!TXSTAbits.TRMT);  // Busy Waiting
+    TXREG = data;             // write to TXREG will send data
 }
 
-
-void UART_Write_Text(char* text) { // Output on Terminal, limit:10 chars
-    for(int i=0;text[i]!='\0';i++)
-        UART_Write(text[i]);
-}
-
-void ClearBuffer(){
-    for(int i = 0; i < 20 ; i++)
+void ClearBuffer() {
+    for (int i = 0; i < STR_MAX; i++)
         mystring[i] = '\0';
     lenStr = 0;
 }
 
-void MyusartRead()
-{
+void MyusartRead() {
     /* TODObasic: try to use UART_Write to finish this function */
-    char data;
-    data = RCREG;
+    char data = RCREG;
     mystring[lenStr++] = RCREG;
-    if(data == '\r')
+    if (data == '\r') {
+        UART_Write('\r');
         UART_Write('\n');
-    UART_Write(data);
-    if(lenStr == 19)
-        lenStr = 0;
-    return ;
+    }else{
+        UART_Write(data);
+    }
+    
 }
 
-char *GetString(){
+char *GetString() {
     return mystring;
 }
 
-
 // void interrupt low_priority Lo_ISR(void)
-void __interrupt(low_priority)  Lo_ISR(void)
-{
-    if(RCIF)
-    {
-        if(RCSTAbits.OERR)
-        {
+void __interrupt(low_priority) Lo_ISR(void) {
+    if (RCIF) {
+        if (RCSTAbits.OERR) {
             CREN = 0;
             Nop();
             CREN = 1;
         }
-        
+
         MyusartRead();
     }
-    
-   // process other interrupt sources here, if required
+
+    // process other interrupt sources here, if required
     return;
 }
