1acc9469da78575eb30bde453f95f500ead032f5
RUEI TING HUNG <rthung96@gmail.com>
2025-01-02 00:47:28 +0800

    feat: Add UART printf support

--- template.c
+++ template.c
@@ -53,11 +53,11 @@
 // CONFIG7H
 #pragma config EBTRB = OFF  // Boot Block Table Read Protection bit (Boot block (000000-0007FFh) not protected from table reads executed in other blocks)
 
+#include <ctype.h>
 #include <pic18f4520.h>
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
 #include <xc.h>
 #define _XTAL_FREQ 4000000
 #define STR_MAX 100
@@ -106,7 +106,6 @@ int GetString(char *str) {
     }
 }
 
-
 void __interrupt(low_priority) Lo_ISR(void) {
     if (RCIF) {
         if (RCSTAbits.OERR) {
@@ -126,44 +125,44 @@ void __interrupt(low_priority) Lo_ISR(void) {
 
 void Initialize(void) {
     // Configure oscillator
-    OSCCONbits.IRCF = 0b110;    // 4 MHz
+    OSCCONbits.IRCF = 0b110;  // 4 MHz
     // Configure ADC
-    TRISAbits.RA0 = 1;          // Set RA0 as input port
-    ADCON1bits.PCFG = 0b1110;   // AN0 as analog input
-    ADCON0bits.CHS = 0b0000;    // Select AN0 channel
-    ADCON1bits.VCFG0 = 0;       // Vref+ = Vdd
-    ADCON1bits.VCFG1 = 0;       // Vref- = Vss
-    ADCON2bits.ADCS = 0b000;    // ADC clock Fosc/2
-    ADCON2bits.ACQT = 0b001;    // 2Tad acquisition time
-    ADCON0bits.ADON = 1;        // Enable ADC
-    ADCON2bits.ADFM = 1;        // Right justified
+    TRISAbits.RA0 = 1;         // Set RA0 as input port
+    ADCON1bits.PCFG = 0b1110;  // AN0 as analog input
+    ADCON0bits.CHS = 0b0000;   // Select AN0 channel
+    ADCON1bits.VCFG0 = 0;      // Vref+ = Vdd
+    ADCON1bits.VCFG1 = 0;      // Vref- = Vss
+    ADCON2bits.ADCS = 0b000;   // ADC clock Fosc/2
+    ADCON2bits.ACQT = 0b001;   // 2Tad acquisition time
+    ADCON0bits.ADON = 1;       // Enable ADC
+    ADCON2bits.ADFM = 1;       // Right justified
 
     // Configure servo (PWM)
-    T2CONbits.TMR2ON = 0b1;     // Timer2 on
-    T2CONbits.T2CKPS = 0b11;    // Prescaler 16
-    CCP1CONbits.CCP1M = 0b1100; // PWM mode
-    PR2 = 0x9b;                 // Set PWM period
-    
+    T2CONbits.TMR2ON = 0b1;      // Timer2 on
+    T2CONbits.T2CKPS = 0b11;     // Prescaler 16
+    CCP1CONbits.CCP1M = 0b1100;  // PWM mode
+    PR2 = 0x9b;                  // Set PWM period
+
     TRISCbits.TRISC2 = 0;
-    
+
     // Configure I/O ports
-    TRISA &= 0xF1;              // Set RA1-RA3 as outputs for LED
-    TRISB = 1;                  // RB0 as input for button
-    TRISC = 0;                  // PORTC as output for servo
-    LATA &= 0xF1;               // Clear RA1-RA3
-    LATC = 0;                   // Clear PORTC
-    
+    TRISA &= 0xF1;  // Set RA1-RA3 as outputs for LED
+    TRISB = 1;      // RB0 as input for button
+    TRISC = 0;      // PORTC as output for servo
+    LATA &= 0xF1;   // Clear RA1-RA3
+    LATC = 0;       // Clear PORTC
+
     // Configure interrupts
-    INTCONbits.INT0IF = 0;      // Clear INT0 flag
-    INTCONbits.INT0IE = 1;      // Enable INT0 interrupt
-    PIE1bits.ADIE = 1;          // Enable ADC interrupt
-    PIR1bits.ADIF = 0;          // Clear ADC flag
-    INTCONbits.PEIE = 1;        // Enable peripheral interrupt
-    INTCONbits.GIE = 1;         // Enable global interrupt
-    RCONbits.IPEN = 1;          //enable Interrupt Priority mode
-    INTCONbits.GIEH = 1;        //enable high priority interrupt
-    INTCONbits.GIEL = 1;        //disable low priority interrupt
-    
+    INTCONbits.INT0IF = 0;  // Clear INT0 flag
+    INTCONbits.INT0IE = 1;  // Enable INT0 interrupt
+    PIE1bits.ADIE = 1;      // Enable ADC interrupt
+    PIR1bits.ADIF = 0;      // Clear ADC flag
+    INTCONbits.PEIE = 1;    // Enable peripheral interrupt
+    INTCONbits.GIE = 1;     // Enable global interrupt
+    RCONbits.IPEN = 1;      // enable Interrupt Priority mode
+    INTCONbits.GIEH = 1;    // enable high priority interrupt
+    INTCONbits.GIEL = 1;    // disable low priority interrupt
+
     // Configure UART
     /*
            TODObasic
@@ -203,32 +202,32 @@ void Initialize(void) {
      RSR   : Current Data
      RCREG : Correct Data (have been processed) : read data by reading the RCREG Register
     */
-    
+
     // Start ADC conversion
     ADCON0bits.GO = 1;
 }
 
 // ---------------- OOP --------------------
 
-int get_LED(){
+int get_LED() {
     return (LATA >> 1);
 }
 
-void set_LED(int value){
+void set_LED(int value) {
     LATA = (value << 1);
 }
 
-void set_LED_separately(int a, int b, int c){
+void set_LED_separately(int a, int b, int c) {
     LATA = (a << 3) + (b << 2) + (c << 1);
 }
 
-void set_LED_analog(int value){
+void set_LED_analog(int value) {
     CCPR1L = (value >> 2);
     CCP1CONbits.DC1B = (value & 0b11);
 }
 
 int current_servo_angle = 0;
-int get_servo_angle(){
+int get_servo_angle() {
     // double duty_cycle = ((CCPR1L << 2) + CCP1CONbits.DC1B) * 4;
     // return (int)((duty_cycle - 500) / (2400 - 500) * 180 - 90);
     return current_servo_angle;
@@ -236,12 +235,14 @@ int get_servo_angle(){
 
 void set_servo_angle(int angle) {
     int current = (CCPR1L << 2) + CCP1CONbits.DC1B;
-    int target = (int)((500 + (double)(angle + 90) / 180 * (2400 - 500)) / 8 / 4) * 8; // angle to pwn
-    
-    while(current != target){
-        if(current < target) current++;
-        else current--;
-        
+    int target = (int)((500 + (double)(angle + 90) / 180 * (2400 - 500)) / 8 / 4) * 8;  // angle to pwn
+
+    while (current != target) {
+        if (current < target)
+            current++;
+        else
+            current--;
+
         CCPR1L = (current >> 2);
         CCP1CONbits.DC1B = (current & 0b11);
         __delay_ms(1);
@@ -249,40 +250,40 @@ void set_servo_angle(int angle) {
     current_servo_angle = angle;
 }
 
-int VR_value_to_servo_angle(int value){
+int VR_value_to_servo_angle(int value) {
     return (int)(((double)value / VR_MAX * 180) - 90);
 }
 
-int VR_value_to_LED_analog(int value){
+int VR_value_to_LED_analog(int value) {
     return value;
 }
 
 void variable_register_changed(int value);
 void button_pressed();
 
-void __interrupt(high_priority) H_ISR(){
-    if(PIR1bits.ADIF){ // Handle variable register interrupt
+void __interrupt(high_priority) H_ISR() {
+    if (PIR1bits.ADIF) {  // Handle variable register interrupt
         int value = (ADRESH << 8) + ADRESL;
         variable_register_changed(value);
         PIR1bits.ADIF = 0;
         __delay_ms(5);
     }
-    
-    if(INTCONbits.INT0IF){ // Handle button interrupt
+
+    if (INTCONbits.INT0IF) {  // Handle button interrupt
         button_pressed();
-        __delay_ms(50); // bouncing problem
+        __delay_ms(50);  // bouncing problem
         INTCONbits.INT0IF = 0;
     }
 }
 
 // --------------- TODO ------------------
 
-void button_pressed(){
+void button_pressed() {
     // Do sth when the button is pressed
-    /* Example: 
+    /* Example:
      * set_LED(get_LED() + 1);
      */
-    
+
     
 }
 
@@ -294,10 +295,10 @@ void variable_register_changed(int value) {  // value: 0 ~ 1023
      * printf("%d\n", value); // print the variable register value on uart terminal
      */
 
-    
+
 }
 
-void keyboard_input(char *str){ // get line from keyboard: this function will be called after you click enter
+void keyboard_input(char *str) {  // get line from keyboard: this function will be called after you click enter
     // Do sth when typing on keyboard
     /* Example:
     for (int i = 0; i < strlen(str); i++) {
@@ -307,36 +308,36 @@ void keyboard_input(char *str){ // get line from keyboard: this function will be
         }
     }
      */
-    
-    
+
+
 }
 
-void main(){
+void main() {
     Initialize();
     /* Usage:
      * set_servo_angle(-90); // input: -90 ~ 90
      * get_servo_angle(); // return value: -90 ~ 90
-     * 
+     *
      * set_LED(5); // 5 = 0b101, set LED1 and LED3 on, LED2 off
      * get_LED(); // return value: an integer, bit 0 -> LED1, bit 1 -> LED2, bit 2 -> LED3
-     * 
+     *
      * set_LED_separately(1, 1, 0); // set LED1 and LED2 on, LED3 off
      * set_LED_analog(512); // input: 0 ~ 1023, represent brightness. NOTICE: LED need to be plugged into the CCP1 pin.
-     * 
+     *
      * VR_value_to_servo_angle(1024); // return value: -90 ~ 90. Change the variable register value to servo angle
      * VR_value_to_LED_analog(1024); // return value: 0 ~ 1024. Change the variable register value to LED brightness
-     * 
+     *
      * delay(1); // delay 1 second
-     * 
+     *
      * printf(); // print on uart terminal
      */
-    
+
     char str[STR_MAX];
-    
-    while(1) {
+
+    while (1) {
         // Do sth in main
-        
-        
+
+
         if (GetString(str)) keyboard_input(str);
         if (ADCON0bits.GO == 0) ADCON0bits.GO = 1;
     }
